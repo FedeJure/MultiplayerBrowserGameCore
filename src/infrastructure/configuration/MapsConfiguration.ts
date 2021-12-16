5e44e86144f99225c7787a6c2cb91ae7c9e28598
Federico Jure <fjure@atixlabs.com>
2021-12-16 20:54:16 -0300

    add id to layer

--- src/infrastructure/configuration/MapsConfiguration.ts
+++ src/infrastructure/configuration/MapsConfiguration.ts
@@ -3,5 +3,5 @@ import { MapConfiguration } from "../../domain/environment/mapConfiguration";
 export const MapsConfiguration: MapConfiguration = {
   singleMapSize: { x: 64, y: 64 },
   patronSizeInPixels: 32,
-  mapLayers: [{mapsInOrder: [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]},{mapsInOrder: [[10, 11]]}],
+  mapLayers: [{id: 0, mapsInOrder: [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]},{id: 1, mapsInOrder: [[10, 11]]}],
 };
