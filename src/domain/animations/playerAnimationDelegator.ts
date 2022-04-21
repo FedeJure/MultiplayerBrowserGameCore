44cf080fcafbe5de8872bc0fc462149b515f40db
Federico Jure <fjure@atixlabs.com>
2022-04-20 22:18:40 -0300

    Finish refactor

--- src/domain/animations/playerAnimationDelegator.ts
+++ src/domain/animations/playerAnimationDelegator.ts
@@ -3,12 +3,11 @@ import { ServerPlayerAnimationDelegator } from "./serverPlayerAnimationDelegator
 
 export class PlayerAnimationDelegator extends ServerPlayerAnimationDelegator {
   constructor(player: Player) {
-    super(player)
+    super(player);
   }
   init(): void {}
   stop(): void {}
   update(time: number, delta: number): void {
-      const currentAnim = this.player.state.anim;
-      this.player.view.playAnimation(currentAnim);
+    this.player.view.playAnimation(this.getAnimation(this.player.state));
   }
 }
