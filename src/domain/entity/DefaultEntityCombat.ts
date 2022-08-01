2b6d629e4249beff777137b7b81887e5e2744959
FedeJure <ae_fjure@idcontractors.net>
2022-08-01 15:59:17 -0300

    add taking damage animation

--- src/domain/entity/DefaultEntityCombat.ts
+++ src/domain/entity/DefaultEntityCombat.ts
@@ -1,17 +1,22 @@
 import { CombatResult } from "../player/combat/combatResult";
+import { AnimationLayer, EntityAnimationCode } from "./animations";
 import { Entity } from "./entity";
 import { EntityCombat } from "./entityCombat";
 
 export class DefaultEntityCombat implements EntityCombat {
   private entity: Entity;
-
+  private life: number = 0
   init(entity: Entity) {
     this.entity = entity;
   }
   update(time: number, delta: number) {
-    this.entity.view.setLifePercent(
-      (this.entity.state.life / this.entity.stats.maxLife) * 100
-    );
+    if (this.life !== this.entity.state.life) {
+      this.entity.view.setLifePercent(
+        (this.entity.state.life / this.entity.stats.maxLife) * 100
+      );
+      this.life = this.entity.state.life
+      this.entity.animations.executeAnimation(EntityAnimationCode.TAKING_DAMAGE, AnimationLayer.COMBAT, false)
+    }
     this.entity.view.setLevel(this.entity.stats.level);
   }
   receiveAttack(attack: CombatResult) {}
--- src/domain/player/movement/clientRemotePlayerMovement.ts
+++ src/domain/player/movement/clientRemotePlayerMovement.ts
@@ -8,16 +8,11 @@ export class ClientRemotePlayerMovement implements EntityMovement {
     this.entity = entity;
   }
   update(time: number, delta: number) {
-    // this.entity.view.setPositionInTime(
-    //   this.entity.state.position.x,
-    //   this.entity.state.position.y,
-    //   DefaultGameConfiguration.playerStatesEventInterval
-    // );
     if (
       Phaser.Math.Distance.BetweenPoints(
         this.entity.view.positionVector,
         this.entity.state.position
-      ) > 20
+      ) > 15
     ) {
       this.entity.view.setPosition(
         this.entity.state.position.x,
